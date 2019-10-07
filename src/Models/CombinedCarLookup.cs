37114fc1d9800b645f5f2416cff7aa2bd3568514
Ben Fidge <ben.fidge@btopenworld.com>
2019-10-07 11:37:11 +0700

    CI Fixes

--- src/Models/CombinedCarLookup.cs
+++ src/Models/CombinedCarLookup.cs
@@ -7,25 +7,9 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-using System.Collections.Generic;
-
 namespace CluedIn.ExternalSearch.Providers.DanishVehicleRegistry.Models
 {
-    public class CombinedCarLookupCollection
-    {
-        public CombinedCarLookupCollection(string name, string dateOfBirth)
-        {
-            Name = name;
-            DateOfBirth = dateOfBirth;
-            Lookups = new List<CombinedCarLookup>();
-        }
-
-        public List<CombinedCarLookup> Lookups { get; set; }
-        public string Name { get; set; }
-        public string DateOfBirth { get; set; }
-    }
-
-    public class CombinedCarLookup
+	public class CombinedCarLookup
     {
         public string Kind { get; set; }
         public string ModelYear { get; set; }
---
+++ src/Models/CombinedCarLookupCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CluedIn.ExternalSearch.Providers.DanishVehicleRegistry.Models
+{
+	public class CombinedCarLookupCollection
+	{
+		public CombinedCarLookupCollection(string name, string dateOfBirth)
+		{
+			Name = name;
+			DateOfBirth = dateOfBirth;
+			Lookups = new List<CombinedCarLookup>();
+		}
+
+		public List<CombinedCarLookup> Lookups { get; set; }
+		public string Name { get; set; }
+		public string DateOfBirth { get; set; }
+	}
+}
